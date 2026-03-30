16be09056b30f484fa4a9d6a966c2922a13b167a
mahdihasanprogrammer <mahdihasan.code@gmail.com>
2026-03-30 19:28:42 +0600

    react project setup , install tailwindcss and daisyUi

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    {/* header  */}
+      <header>
+
+      </header>
+
+
+      {/* main section */}
+      <main>
+
+      </main>
+
+      {/* footer section */}
+      <footer>
+
+      </footer>
+    </>
+  )
+}
+
+export default App
