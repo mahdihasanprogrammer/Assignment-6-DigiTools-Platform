46e08f4e12ccc41dbdea5548d943cc5e8233d69f
mahdihasanprogrammer <mahdihasan.code@gmail.com>
2026-03-31 23:19:39 +0600

    added functionality: show total price in all products of cart, delete product from cart tab when clicked delete button ,  delete all product from cart tab when clicked proceed checkout button

--- src/components/MainSection/DigitalToolsSection/Carts/CartsData.jsx
+++ src/components/MainSection/DigitalToolsSection/Carts/CartsData.jsx
@@ -1,22 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SingleCart from './SingleCart';
 import cartIcon from '../../../../assets/assets/products/shopping-cart.png'
+import { toast } from 'react-toastify';
 
-const CartsData = ({selectedCarts}) => {
+
+const CartsData = ({selectedCarts,setSelectedCarts}) => {
+
+const [totalPrice, setTotalPrice] = useState(0);
+
+
+// delete all product from cart tab when clicked proceed to checkout btn;
+const deleteAllCartFromCartTab = ()=>{
+    setSelectedCarts([])
+    toast.warning('deleted all products from  cart');
+}
+
+
     return (
-        <div className='shadow-lg rounded-2xl space-y-5 p-8 mt-10 border-2 border-gray-200'>
+        // cart tab container;
+        <div className='shadow-lg rounded-2xl space-y-5 p-3 md:p-8 mt-10 border-2 border-gray-200'>
 
             <h1 className='text-xl font-semibold'>Your Cart</h1>
+
+            {/* check condition, and show product in cart using map method */}
             { selectedCarts.length > 0?
                 selectedCarts.map(cart =>
-                    <SingleCart key={cart.id} cart={cart} />) 
+                    <SingleCart
+                    setSelectedCarts={setSelectedCarts}
+                    setTotalPrice={setTotalPrice}
+                     selectedCarts={selectedCarts}
+                     key={cart.id} cart={cart} />)
 
 
+                    //  show this container when cart is empty;
                     : <div className='py-20 text-center flex flex-col justify-center items-center gap-2'> 
                         <img className='size-6' src={cartIcon} alt="" />
                         <h1 className='font-medium text-gray-500'>Your cart is empty</h1>
                     </div>
             }
+
+            {/* check condition,  show  total price of cart, and proceed checkout btn*/}
+                {
+                    selectedCarts.length >0 && 
+                    <div className='space-y-5'>
+                        <h1 
+                        className='flex justify-between items-center text-xl font-semibold px-5'>
+                            Total Price: <span className='text-2xl'>
+                                ${totalPrice}</span>
+                        </h1>
+
+
+                        <button onClick={deleteAllCartFromCartTab}
+                        className='bg-linear-to-r from-indigo-500 to bg-purple-500 text-white w-full rounded-full text-lg  py-2 active:translate-y-0.5 transition-all duration-200 cursor-pointer '>
+                            Proceed to checkout
+                        </button>
+                    </div>
+                }
         </div>
     );
 };
--- src/components/MainSection/DigitalToolsSection/Carts/SingleCart.jsx
+++ src/components/MainSection/DigitalToolsSection/Carts/SingleCart.jsx
@@ -1,24 +1,40 @@
 import React from 'react';
 import { RiDeleteBin6Line } from "react-icons/ri";
+import { toast } from 'react-toastify';
 
-const SingleCart = ({cart}) => {
+const SingleCart = ({cart,selectedCarts, setSelectedCarts, setTotalPrice}) => {
+
+
+// click the delete button, and delete cart form cart tab;
+const handleDeleteCartFromCartTab =(cart)=>{
+    const filteredCart = selectedCarts.filter(filterCart => filterCart.id !== cart.id);
+    toast.warning('deleted product from cart ')
+   setSelectedCarts(filteredCart)
+}
+
+// set total price of tab carts
+const totalCartPrice =  selectedCarts.reduce((sum, selectedItem)=> sum + selectedItem.price,0);
+
+// updated totalPrice state;
+setTotalPrice(totalCartPrice);
+
     return (
-
-        
-        <div className='flex justify-between items-center p-5 rounded-lg bg-gray-100'>
+        <div className='p-4 rounded-lg bg-gray-100 flex justify-between items-center'>
            
-           <div className='flex items-center gap-5'>
-                 <img className='p-3 rounded-full shadow bg-white' src={cart.icon} alt="" />
+           {/* left content */}
+           <div className='flex items-center gap-3 md:gap-5'>
+                 <img className='size-6 p-2 md:size-auto box-content md:box-border md:p-3 rounded-full shadow bg-white' src={cart.icon} alt="" />
                 <div>
-                    <h1 className='text-xl font-semibold'>{cart.name}</h1>
+                    <h1 className='md:text-xl font-semibold'>{cart.name}</h1>
                     <p className='font-medium'>${cart.price}</p>
                 </div>
            </div>
 
-           <div className='text-red-500 bg-white rounded-full p-2 cursor-pointer shadow'>
-                <RiDeleteBin6Line className='size-6' />
-           </div>
-
+            {/* right content */}
+           <button onClick={() => {handleDeleteCartFromCartTab(cart)}}
+            className='text-red-500 bg-white rounded-full p-2 cursor-pointer shadow'>
+                <RiDeleteBin6Line className='size-4 md:size-6' />
+           </button>
         </div>
     );
 };
